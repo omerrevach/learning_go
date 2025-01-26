608b8068b72a05e29af8db1a18b1b31a4fcae265
root <hairviction@gmail.com>
2025-01-26 21:09:49 +0200

    learned structs

---
+++ tutorials/learn/structs/main.go
@@ -0,0 +1,58 @@
+// type struct_name struct {
+// 	member1 datatype;
+// 	member2 datatype;
+// 	member3 datatype;
+// 	...
+//   } 
+
+package main
+
+import (
+	"fmt"
+)
+
+// declare a struct type Person with the following members: name, age, job and salary:
+type Person struct {
+	name string
+	age int
+	job string
+	salary int
+}
+
+func main() {
+	var person1 Person
+	var person2 Person
+
+	// person1 specifications
+	person1.name = "omer"
+	person1.age = 22
+	person1.job = "DevOps Engineer"
+	person1.salary = 0
+
+	// person2 specifications
+	person2.name = "bobby"
+	person2.age = 15
+	person2.job = "Homeless"
+	person2.salary = 50000
+
+	fmt.Println("Name: ", person1.name)
+	fmt.Println("Age: ", person1.age)
+	fmt.Println("Job: ", person1.job)
+	fmt.Println("Salary: ", person1.salary)
+
+	fmt.Println("\nName: ", person2.name)
+	fmt.Println("Age: ", person2.age)
+	fmt.Println("Job: ", person2.job)
+	fmt.Println("Salary: ", person2.salary)
+
+	printPerson(person1)
+	printPerson(person2)
+}
+
+// to print it as a funtion
+func printPerson(person Person) {
+	fmt.Println("\nName: ", person.name)
+	fmt.Println("Age: ", person.age)
+	fmt.Println("Job: ", person.job)
+	fmt.Println("Salary: ", person.salary)
+  }
